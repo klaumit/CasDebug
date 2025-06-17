243bc740f9ea95d4e59fad6c059f917c162b43d2
Klaus Mittermann <klaus.mittermann@hotmail.com>
2025-06-17 22:06:37 +0200

    Update build.bat

--- Complex/build.bat
+++ Complex/build.bat
@@ -2,8 +2,6 @@
 
 set PATH=c:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin;%PATH%
 
-dotnet build Complex.sln
-
 msbuild Complex.sln /nologo /clp:ErrorsOnly
 
 dotnet build Complex.sln
